768778af53811f201cc224f1a59bb03fa8e35851
Gabriel Laroche <gabriel.laroche@nurun.com>
2020-10-26 15:55:18 -0400

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        // Initialize Fathom when the app loads
+        Fathom.load('ILNJCBYG', {
+            includedDomains: ['antelope.gabriellaroche.dev'],
+            excludedDomains: ['localhost']
+        });
+
+        function onRouteChangeComplete() {
+            Fathom.trackPageview();
+        }
+        // Record a pageview when route changes
+        router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+        // Unassign event listener
+        return () => {
+            router.events.off('routeChangeComplete', onRouteChangeComplete);
+        }
+    }, []);
+
   return <Component {...pageProps} />
 }
 
