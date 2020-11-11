afcfa2cc5cdcb36af7efaa6348aa06db5f6a0003
Gabriel Laroche <gabriel.laroche@nurun.com>
2020-11-11 09:09:22 -0500

    Add redirect for death to ie11

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/ie11-death-countdown',
+        destination: 'https://death-to-ie11.com/',
+        permanent: true,
+      },
+    ]
+  },
+}
